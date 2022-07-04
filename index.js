450237782d387360f0e747c7219a55bc4c19ce0d
thefrostyausty <austinrobinson0000@gmail.com>
2022-07-04 01:48:17 -0400

    Creating variables

---
+++ index.js
@@ -0,0 +1,6 @@
+const balloon = document.getElementById('air-balloon')
+const cloudOne = document.getElementById('cloud-one')
+const cloudTwo = document.getElementById('cloud-two')
+const cloudThree = document.getElementById('cloud-three')
+const cloudFour = document.getElementById('cloud-four')
+const cloudFive = document.getElementById('cloud-five')
