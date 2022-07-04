9b94d3da5ab2560ec645a440555af6eb2b720e00
thefrostyausty <austinrobinson0000@gmail.com>
2022-07-04 04:53:23 -0400

    Letters added, blurb added and adjusting margins

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const balloon = document.getElementById('air-balloon')
-const cloudOne = document.getElementById('cloud-one')
-const cloudTwo = document.getElementById('cloud-two')
-const cloudThree = document.getElementById('cloud-three')
-const cloudFour = document.getElementById('cloud-four')
-const cloudFive = document.getElementById('cloud-five')
-const cloudSix = document.getElementById('cloud-six')
+const letterA = document.getElementById('letter-a')
+const letterU = document.getElementById('letter-u')
+const letterS = document.getElementById('letter-s')
+const letterT = document.getElementById('letter-t')
+const letterI = document.getElementById('letter-i')
+const letterN = document.getElementById('letter-n')
 const javascript = document.getElementById('javascript')
 const react = document.getElementById('react')
 const graphql = document.getElementById('graphql')
@@ -16,18 +16,18 @@ function move() {
  
   balloon.style.bottom = 10 + incrementer * 0.1 + '%'
 
-  cloudOne.style.bottom = 60 + incrementer * 0.12 + '%'
-  cloudOne.style.left = 15 + incrementer * 0.1 + '%'
-  cloudTwo.style.bottom = 60 + incrementer * 0.14 + '%'
-  cloudTwo.style.left = 25 + incrementer * 0.15 + '%'
-  cloudThree.style.bottom = 60 + incrementer * 0.1 + '%'
-  cloudThree.style.left = 35 + incrementer * -0.12 + '%'
-  cloudFour.style.bottom = 60 + incrementer * 0.16 + '%'
-  cloudFour.style.left = 45 + incrementer * -0.15 + '%'
-  cloudFive.style.bottom = 60 + incrementer * 0.2 + '%'
-  cloudFive.style.left = 55 + incrementer * 0.16 + '%'
-  cloudSix.style.bottom = 60 + incrementer * 0.2 + '%'
-  cloudSix.style.left = 65 + incrementer * 0.125 + '%'
+  letterA.style.bottom = 60 + incrementer * 0.12 + '%'
+  letterA.style.left = 15 + incrementer * 0.1 + '%'
+  letterU.style.bottom = 60 + incrementer * 0.14 + '%'
+  letterU.style.left = 25 + incrementer * 0.15 + '%'
+  letterS.style.bottom = 60 + incrementer * 0.1 + '%'
+  letterS.style.left = 35 + incrementer * -0.12 + '%'
+  letterT.style.bottom = 60 + incrementer * 0.16 + '%'
+  letterT.style.left = 45 + incrementer * -0.15 + '%'
+  letterI.style.bottom = 60 + incrementer * 0.2 + '%'
+  letterI.style.left = 55 + incrementer * 0.16 + '%'
+  letterN.style.bottom = 60 + incrementer * 0.2 + '%'
+  letterN.style.left = 65 + incrementer * 0.125 + '%'
   javascript.style.left = 20 + incrementer * -2.2 + '%'
   react.style.left = 26 + incrementer * -1.5 + '%'
   graphql.style.left = 32 + incrementer * -1 + '%'
